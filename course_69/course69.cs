dea6fc82f41150ebacbfbcf5ddaaf8ffb567d33c
Nawab Zadi <nawabzadi0819@gmail.com>
2024-04-14 23:54:14 +0500

    course 67

--- course_69/course69.cs
+++ course_69/course69.cs
@@ -5,15 +5,8 @@ using System.Text;
 
 public class optionalParameter
 {
-    //four ways that can be used to make parameters optional
-    //1. use parameter arrays
-    //2. use method overriding
-    //3. specify parameters defaults
-    //4. use OptionalAttribute that is present in System.Runtime.InteropServices namespace
     public static void Main()
     {
-        AddNumbers(10, 20);
-        AddNumbers(10, 20, new object[] {11, 22, 33});
         AddNumbers(10, 90, 89, 88, 90); // Calling the method with more than required parameters
     }
     //params should be at last
